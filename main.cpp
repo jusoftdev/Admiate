4f5a394a32253b533d5e0cfa93e3c1f976fd7436
Terraminator <erik1wilhelm@gmail.com>
2021-12-13 16:46:44 +0100

    update

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
 #include "network_scan.hpp"
 
 
+int main() {
+	Network_Scan netscan("192.168.178.0");
+	vector<string>devices = netscan.search_for_devices();
+	for (int i{ 0 }; i < sizeof(devices); i++) {
+		cout << devices[i] << endl;
+	}
+}
